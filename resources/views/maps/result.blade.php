f3c99df75318932ec11ada89ff61966e936c4982
hemish7 <hemishpanchal7@gmail.com>
2020-11-06 16:08:09 +0530

    checkbox changed

--- resources/views/maps/result.blade.php
+++ resources/views/maps/result.blade.php
@@ -75,10 +75,19 @@
                                             <div class="geodir-post-content-container">
                                                 <div class="geodir_post_meta geodir-field-post_content" style='max-height:120px;overflow:hidden;'>{{ $place->description }} <a href="place/{{$place->place_id}}" class='gd-read-more  gd-read-more-fade' style="font-size: medium">Read more...</a></div>
                                             </div>
-                                            <br><br>
-                                            <div class="check">
-                                                <span><input type="checkbox" name="checkbox[]" value="{{$place->place_id}}"></span>
+                                            
+                                            <div class="grid">
+                                                
+                                                <label class="checkbox path">
+
+                                                    <input type="checkbox" name="checkbox[]" value="{{$place->place_id}}">
+                                                    <svg viewBox="0 0 21 21">
+                                                        <path d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186"></path>
+                                                    </svg>
+                                                </label>
+
                                             </div>
+                                            <br>
                                         </div>
                                     </li>
                                     <?php
